32a664d754cd64fe0b17e38949f63a1e32ad50dd
Simon Wahlström <simon.otdw@gmail.com>
2020-03-27 10:43:53 +0100

    template handler

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 
+from src.template_handler.template_handler import TemplateHandler
+
+th = TemplateHandler()
+th.load_template_generators("template_generators/main.txt")
+th.generate_templates()
+th.save_templates("templates/main.tmpl")
---
+++ src/template_handler/template_handler.py
@@ -0,0 +1,77 @@
+import itertools
+import re
+
+class TemplateHandler():
+
+	def __init__(self):
+		self.template_generators = [] # Template Generators represented as splitted lists
+		self.templates = []
+
+	def load_template_generators(self, path):
+		"""Load Generators used to create all template permutations"""
+		with open(path, "r") as file:
+			lines = file.read().split("\n")
+			for line in lines:
+				if not line.startswith("#") and not line == "":
+					self.template_generators.append(line.split(" "))
+
+	def generate_templates(self):
+		"""
+		Generate template permutations from loaded template generators.
+		"""
+		for generator in self.template_generators:
+			self.templates.extend(self.expand(generator))
+
+
+	def save_templates(self, path):
+		"""Save loaded/generated templates to file"""
+		with open(path, "w+") as file:
+			for template in self.templates:
+				template_str = ' '.join(template) + "\n"
+				file.write(template_str)
+
+
+	def load_existing_templates(self, path):
+		"""Load previously generated templates"""
+		pass
+
+
+	def expand(self, template):
+		"""
+		Gather permutations from a specific Template Generator.
+		"""
+
+		# Make each element a list so we can figure out permutations.
+		# Ex: [if, var<int>, {{<|>}}, int] would become [[if], [var<int>], [<, >], [int]]
+		instruction_parts = []
+		for element in template:
+			if not "{{" in element:
+				# Just one option of token
+				instruction_parts.append([element])
+
+			else:
+				# Multiple options of tokens.
+				# Find all permutations of the element.
+				# func(|avar<int>) becomes [func(), func(avar<int>)]
+				
+				dynamic_part = re.findall(r"{{(.*)}}", element)[0]
+				static_part = re.sub(r"{{.*}}", "???", element)
+
+				tokens = []
+				for token in dynamic_part.split("|"):
+					tokens.append(token)
+
+				# Put in each avaialable dynamic part of the element into the static part.
+				element_permutations = []
+				for token in tokens:
+					permutation = re.sub(r"\?\?\?", token, static_part)
+					element_permutations.append(permutation)
+
+				instruction_parts.append(element_permutations)
+
+		# Now find all permutations of the template
+		permutations = []
+		for template_permutation in itertools.product(*instruction_parts):
+			permutations.append(list(template_permutation))
+
+		return permutations
